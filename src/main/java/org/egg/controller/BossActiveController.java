721ed6551b05258eb7b98f0bbd963cf61bf5439f
egg_CN <1608384702@qq.com>
2020-02-28 16:52:19 +0800

    debug

---
+++ src/main/java/org/egg/controller/BossActiveController.java
@@ -0,0 +1,34 @@
+package org.egg.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author dataochen
+ * @Description
+ * @date: 2020/2/28 15:52
+ */
+@RequestMapping("/boss/active")
+@Controller
+@Slf4j
+public class BossActiveController {
+
+    /**
+     * 寝室组队活动管理页
+     */
+
+    /**
+     * 查询中奖名单
+     * 入参：中奖类型 ONE/TWO
+     */
+
+    /**
+     * 发放奖品名额
+     */
+
+    /**
+     * 寝室组队数据集合查询
+     *
+     */
+}
